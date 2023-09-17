017de4605b36ddaa6d5543f0f1cd140f1c22cefc
yevhenporohniaviy <porohniaviy.yevheni@gmail.com>
2023-09-17 15:45:47 +0300

    Setup

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  runtimeConfig: {
+    OPENAI_API_KEY: process.env.OPENAI_API_KEY
+  },
+  modules: [
+    '@nuxtjs/tailwindcss',
+    'nuxt-icon'
+  ],
   devtools: { enabled: true }
 })
